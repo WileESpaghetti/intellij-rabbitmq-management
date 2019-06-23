fd12f5af04f94a51c8c8a05cb4197398829fe319
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-22 20:30:50 -0500

    Allow configuration dialog to be opened

--- src/main/java/com/github/users/wileespaghetti/rabbitmq/actions/testing/OpenRabbitmqConfigurationDialog.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/actions/testing/OpenRabbitmqConfigurationDialog.java
@@ -1,12 +1,14 @@
 package com.github.users.wileespaghetti.rabbitmq.actions.testing;
 
+import com.github.users.wileespaghetti.rabbitmq.view.ui.RabbitmqConnectionManagerDialog;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class OpenRabbitmqConfigurationDialog extends AnAction {
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        RabbitmqConnectionManagerDialog.showDialog();
     }
 }
