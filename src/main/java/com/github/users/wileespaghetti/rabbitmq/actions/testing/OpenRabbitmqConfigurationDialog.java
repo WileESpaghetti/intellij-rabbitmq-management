8fce4082dd5c98a464739418b1a0c2a3f540bb46
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-22 20:02:27 -0500

    Add "RabbitMQ Configuration" menu item to tools menu

---
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/actions/testing/OpenRabbitmqConfigurationDialog.java
@@ -0,0 +1,12 @@
+package com.github.users.wileespaghetti.rabbitmq.actions.testing;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class OpenRabbitmqConfigurationDialog extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
