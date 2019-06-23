eeb5d02a2e68945f38b343ba75932b1320008822
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-23 17:44:24 -0500

    Add content for empty right panel

--- src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/RabbitmqConfigEditorImpl.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/RabbitmqConfigEditorImpl.java
@@ -1,9 +1,14 @@
 package com.github.users.wileespaghetti.rabbitmq.view.ui;
 
+import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.ui.OnePixelSplitter;
+import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.components.panels.NonOpaquePanel;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -30,11 +35,26 @@ public class RabbitmqConfigEditorImpl<Settings> extends SettingsEditor<Settings>
         rootPanelPreferredSize.width = Math.max(rootPanelPreferredSize.width, 800);
         rootPanelPreferredSize.height = Math.max(rootPanelPreferredSize.height, 600);
         this.myRootPanel.setPreferredSize(rootPanelPreferredSize);
+
+        this.setEmptyRightPanel();
     }
 
     @Override
     protected void resetEditorFrom(@NotNull Settings s) {
 
+    }
+
+    private void setEmptyRightPanel() {
+        this.myRightPanel.removeAll();
+
+        JPanel emptyRightPanelText = new JPanel(new FlowLayout(0, 0, 0));
+        emptyRightPanelText.setBorder(JBUI.Borders.empty(10));
+        emptyRightPanelText.add(new JLabel("Please add a connection to configure"));
+
+        JScrollPane var4 = ScrollPaneFactory.createScrollPane(emptyRightPanelText, true);
+        this.myRightPanel.add(var4, BorderLayout.CENTER);
+
+        UIUtil.setupEnclosingDialogBounds(this.myRootPanel);
     }
 
     private JComponent createLeftPanel() {
