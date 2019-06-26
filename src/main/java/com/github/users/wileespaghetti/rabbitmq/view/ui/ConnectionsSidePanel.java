fe462e14de254792d27234ca38611cd8b5647d92
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-25 21:21:19 -0500

    Allow left sidebar to scroll

--- src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/ConnectionsSidePanel.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/ConnectionsSidePanel.java
@@ -1,5 +1,8 @@
 package com.github.users.wileespaghetti.rabbitmq.view.ui;
 
+import com.intellij.ui.ScrollPaneFactory;
+import com.intellij.ui.components.GradientViewport;
+import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
@@ -8,8 +11,19 @@ import java.awt.*;
 // com.intellij.database.view.ui.DataSourceSidePanel
 public class ConnectionsSidePanel {
     private final JPanel myPanel = new JPanel(new BorderLayout());
+    private final JScrollPane myScroll;
 
     public ConnectionsSidePanel() {
+        this.myScroll = ScrollPaneFactory.createScrollPane(null, true);
+
+        this.myScroll.setViewport(new GradientViewport(new JLabel("Gradient Content"), JBUI.insetsTop(5), true));
+        this.myScroll.setCorner(ScrollPaneConstants.LOWER_RIGHT_CORNER, null);
+        this.myScroll.setCorner(ScrollPaneConstants.LOWER_LEFT_CORNER, null);
+        this.myScroll.setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, null);
+        this.myScroll.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, null);
+
+        this.myPanel.add(myScroll, BorderLayout.CENTER);
+
         UIUtil.setBackgroundRecursively(this.myPanel, UIUtil.SIDE_PANEL_BACKGROUND);
     }
 
