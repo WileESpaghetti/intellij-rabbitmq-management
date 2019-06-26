38f01075734049055cf8cac8710b9dcef05f0293
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-26 18:34:46 -0500

    Add list model for sidebar

--- src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/ConnectionsSidePanel.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/view/ui/ConnectionsSidePanel.java
@@ -1,19 +1,27 @@
 package com.github.users.wileespaghetti.rabbitmq.view.ui;
 
 import com.intellij.ui.ScrollPaneFactory;
+import com.intellij.ui.SeparatorWithText;
 import com.intellij.ui.components.GradientViewport;
+import com.intellij.ui.components.JBList;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 // com.intellij.database.view.ui.DataSourceSidePanel
 public class ConnectionsSidePanel {
     private final JPanel myPanel = new JPanel(new BorderLayout());
     private final JScrollPane myScroll;
+    private final JList myList;
+    private final DefaultListModel myModel = new DefaultListModel();
+    private final List<SeparatorWithText> mySeparators = ContainerUtil.newArrayList();
 
     public ConnectionsSidePanel() {
+        this.myList = new JBList(this.myModel);
         this.myScroll = ScrollPaneFactory.createScrollPane(null, true);
 
         this.myScroll.setViewport(new GradientViewport(new JLabel("Gradient Content"), JBUI.insetsTop(5), true));
@@ -24,10 +32,21 @@ public class ConnectionsSidePanel {
 
         this.myPanel.add(myScroll, BorderLayout.CENTER);
 
+        this.myList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
         UIUtil.setBackgroundRecursively(this.myPanel, UIUtil.SIDE_PANEL_BACKGROUND);
     }
 
     public JComponent getComponent() {
         return this.myPanel;
+    }
+
+    public JList getList() {
+        return this.myList;
+    }
+
+    public void clear() {
+        this.myModel.clear();
+        this.mySeparators.clear();
     }
 }
