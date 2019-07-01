48d9b1da8d52b07bd059feaa36f4c47010ab970b
Lehman Black <WileESpaghetti@users.noreply.github.com>
2019-06-30 20:28:56 -0500

    Process connection type events

--- src/main/java/com/github/users/wileespaghetti/rabbitmq/connectionType/ConnectionTypeManager.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/connectionType/ConnectionTypeManager.java
@@ -1,9 +1,17 @@
 package com.github.users.wileespaghetti.rabbitmq.connectionType;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.components.ServiceManager;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class ConnectionTypeManager {
     public static ConnectionTypeManager getInstance() {
         return ServiceManager.getService(ConnectionTypeManager.class);
     }
+
+    public abstract void updateConnectionType(@NotNull ConnectionType connectionType);
+
+    public abstract void removeConnectionType(@NotNull ConnectionType connectionType);
+
+    public abstract void addConnectionTypeListener(@NotNull ConnectionTypeListener listener, @NotNull Disposable disposable);
 }
--- src/main/java/com/github/users/wileespaghetti/rabbitmq/connectionType/ConnectionTypeManagerImpl.java
+++ src/main/java/com/github/users/wileespaghetti/rabbitmq/connectionType/ConnectionTypeManagerImpl.java
@@ -1,4 +1,52 @@
 package com.github.users.wileespaghetti.rabbitmq.connectionType;
 
+import com.intellij.openapi.Disposable;
+import com.intellij.util.EventDispatcher;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
 public class ConnectionTypeManagerImpl extends ConnectionTypeManager {
+    private final EventDispatcher<ConnectionTypeListener> myDispatcher;
+    private final Map<String, ConnectionType> myConnectionTypes;
+
+    public ConnectionTypeManagerImpl() {
+        this.myConnectionTypes = ContainerUtil.newLinkedHashMap();
+        this.myDispatcher = EventDispatcher.create(ConnectionTypeListener.class);
+    }
+
+    @NotNull
+    private ConnectionType addConnectionType(@NotNull ConnectionType connectionType) {
+        this.myConnectionTypes.put(connectionType.getId(), connectionType);
+        this.getDispatcher().getMulticaster().connectionTypeAdded(connectionType);
+
+        return connectionType;
+    }
+
+    @Override
+    public void updateConnectionType(@NotNull ConnectionType connectionType) {
+        if (!this.myConnectionTypes.containsKey(connectionType.getId())) {
+            this.addConnectionType(connectionType);
+        }
+    }
+
+    @Override
+    public void removeConnectionType(@NotNull ConnectionType connectionType) {
+        ConnectionType removedConnectionType = this.myConnectionTypes.remove(connectionType);
+        if (removedConnectionType != null) {
+            this.getDispatcher().getMulticaster().connectionTypeRemoved(removedConnectionType);
+        }
+
+    }
+
+    @Override
+    public void addConnectionTypeListener(@NotNull ConnectionTypeListener listener, @NotNull Disposable disposable) {
+        this.myDispatcher.addListener(listener, disposable);
+    }
+
+    EventDispatcher<ConnectionTypeListener> getDispatcher() {
+        return this.myDispatcher;
+    }
+
 }
